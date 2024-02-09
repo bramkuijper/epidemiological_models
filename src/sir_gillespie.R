c1366db7617c53c998715ffcf14e6426c0b43dab
megabyte22 <a.l.w.kuijper@exeter.ac.uk>
2024-02-09 08:01:46 +0000

    R scripts for an epidemiology course

---
+++ src/sir_gillespie.R
@@ -0,0 +1,17 @@
+# SIR model Gillespie
+# see http://homepages.warwick.ac.uk/~masfz/ModelingInfectiousDiseases/Chapter6/Program_6.4/Program_6_4.py 
+
+
+def stoch_eqns(IN)
+
+
+
+def SIR_Gillespie(beta=1.0, 
+                  gamma=1.0/10.0, 
+                  mu=5e-4, 
+                  N0=5000.0,
+                  max_time=2*365)
+{
+    # initial number of infecteds
+    
+} # end SIR_Gillespie
